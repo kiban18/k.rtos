ef4d9e814326f6a0faafa5b8cdf28fd6826528c2
Ki-Hwan Lee <kiban18@gmail.com>
2013-10-02 16:38:10 +0900

    trivial

--- kernel/mutex.c
+++ kernel/mutex.c
@@ -28,8 +28,6 @@ int MutexLock(struct MutexStruct *mutex) {
 #if defined (_DEBUG_SHOW_SEMAPHORE_USING_MUTEX_)
         printf("**************************************** [MutexLock] mutex(0x%08x) is already locked by owner(%d), this task(%d) is Waiting\n",
                 mutex, mutex->owner->timeQuantum, currentTask->timeQuantum);
-        printWaitQ(mutex->waitQ);
-        printAllReadyQ();
 #endif // DEBUG
         TaskWait(&mutex->waitQ, currentTask);
         IntRestoreIRQ(flag);
@@ -54,8 +52,6 @@ int MutexUnlock(struct MutexStruct *mutex) {
 
 #if defined (_DEBUG_SHOW_SEMAPHORE_USING_MUTEX_)
     printf("**************************************** [MutexUnlock] mutex(0x%08x) is about to unlock from this task(%d)\n", mutex, currentTask->timeQuantum);
-    printWaitQ(mutex->waitQ);
-    printAllReadyQ();
 #endif // DEBUG
 
     flag = IntSaveDisableIRQ();
@@ -69,8 +65,6 @@ int MutexUnlock(struct MutexStruct *mutex) {
 
 #if defined (_DEBUG_SHOW_SEMAPHORE_USING_MUTEX_)
     printf("**************************************** [MutexUnlock] mutex(0x%08x) is unlocked\n", mutex);
-    printWaitQ(mutex->waitQ);
-    printAllReadyQ();
 #endif // DEBUG
 
     return 0;
